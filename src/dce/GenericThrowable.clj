2140b37a5478e406bfa5494faf6e4ed995332bcc
Stephen C. Gilardi <scgilardi@gmail.com>
2011-06-12 21:53:47 +0000

    first cut at generic throwable

--- src/dce/GenericThrowable.clj
+++ src/dce/GenericThrowable.clj
@@ -2,50 +2,51 @@
   (:gen-class :extends Throwable
               :state state
               :init init
-              :constructors {[Object Object Object]
+              :constructors {[Object Object]
                              []}))
 
 (defn -init
-  [obj form env]
-  [[] {:obj obj :form form :env env}])
+  [obj env]
+  [[] {:obj obj :env env}])
 
 (defn -toString [self]
-  (format "#<GenericThrowable dce.GenericThrowable: %s"
-          (:obj (.state self))))
+  (str (.getCanonicalName (class self)) ": " (-> self .state :obj)))
 
 (defmacro throw+ [obj]
-  `(throw (dce.GenericThrowable. ~obj {} {})))
+  `(throw (dce.GenericThrowable. ~obj (zipmap '~(keys &env) [~@(keys &env)]))))
 
 (defn- catch-form? [x]
   (and (seq? x) (= 'catch (first x))))
+
+(defn- type-name? [x]
+  (or (keyword? x)
+      (and (symbol? x) (class? (resolve x)))))
 
 (defmacro try+
   [& body]
   (let [catch-clauses (filter catch-form? body)
         try-body (remove catch-form? body)
         throwable (gensym)
-        state (gensym)
-        stack (gensym)
-        thrown (gensym)
-        &thrown-form (gensym)
-        &thrown-env (gensym)]
+        thrown (gensym)]
     `(try
        ~@try-body
        (catch Throwable ~throwable
-         (let [[~thrown ~&thrown-form ~&thrown-env]
-               (if (instance? dce.GenericThrowable ~throwable)
-                 (let [~state (.state ~throwable)
-                       ~stack (drop 3 (.getStackTrace ~throwable))]
-                   [(:obj ~state)
-                    (vary-meta (:form ~state) assoc :stack ~stack)
-                    (:env ~state)])
-                 [~throwable])]
+         (let [~thrown (if (instance? dce.GenericThrowable ~throwable)
+                         (-> ~throwable .state :obj)
+                         ~throwable)
+               ~'&thrown-context
+               (when (instance? dce.GenericThrowable ~throwable)
+                 (hash-map
+                  :env (-> ~throwable .state :env)
+                  :stack (into-array (drop 3 (.getStackTrace ~throwable)))))]
            (cond
             ~@(mapcat
-               (fn [[_  type local-name & catch-body]]
-                 `[(isa? (type ~thrown) ~type)
-                   (let [~local-name ~thrown]
+               (fn [[_ type-or-pred local-name & catch-body]]
+                 [(if (type-name? type-or-pred)
+                    `(isa? (type ~thrown) ~type-or-pred)
+                    `(~type-or-pred ~thrown))
+                  `(let [~local-name ~thrown]
                      ~@catch-body)])
                catch-clauses)
-            :else (throw ~throwable)))))))
-
+            :else
+            (throw ~throwable)))))))
---
+++ src/dce/foo.clj
@@ -0,0 +1,10 @@
+(ns dce.foo
+  (:use dce.GenericThrowable))
+
+(defn test-func [x y]
+  (try+ (let [a 1 b 2]
+          (throw+ :hi))
+        (catch keyword? e
+          (prn e)
+          (prn &thrown-context)
+          (clojure.pprint/pprint (:stack &thrown-context)))))
